719e16f74ffe0a86aab153e123a8de05f3a14843
Dimitry <xxhellfire69xx@gmail.com>
2025-10-14 15:47:59 +0200

    WIP on feature
    Base push

--- backend/tickets/models.py
+++ backend/tickets/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 import uuid
 import qrcode
 from io import BytesIO
