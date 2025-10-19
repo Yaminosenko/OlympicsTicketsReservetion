5aa1bbbb93dfdff9bd61dffd19927e499a010a42
Dimitry <xxhellfire69xx@gmail.com>
2025-10-19 18:33:00 +0200

    Fix Migration
    Variable Django_settings_module

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
